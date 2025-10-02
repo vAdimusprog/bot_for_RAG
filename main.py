802469d16f28b2df848851caeb0478b1055cd724
vAdimusprog <bikeev_2003@mail.ru>
2025-10-02 16:08:19 +0300

    bot for tg

---
+++ main.py
@@ -0,0 +1,24 @@
+import asyncio
+from bot import bot, dp, scheduler
+from start import start_router
+# from work_time.time_func import
+from aiogram.types import BotCommand, BotCommandScopeDefault
+
+async def set_commands():
+    commands = [BotCommand(command='start', description='Старт'),
+                BotCommand(command='start_2', description='Старт 2'),
+                BotCommand(command='start_3', description='Старт 3')]
+    await bot.set_my_commands(commands, BotCommandScopeDefault())
+    print("Бот запущен и команды установлены!")
+
+async def main():
+    # scheduler.add_job(send_time_msg, 'interval', seconds=10)
+    # scheduler.start()
+    dp.include_router(start_router)
+    await set_commands()
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ start.py
@@ -0,0 +1,30 @@
+from aiogram import Router, F
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+from all_kb import main_kb, create_spec_kb
+from aiogram.filters import CommandStart, Command, CommandObject
+
+start_router = Router()
+
+@start_router.message(CommandStart())
+async def cmd_start(message: Message, command: CommandObject):
+    command_args: str = command.args
+    if command_args:
+        await message.answer(
+            f'Запуск сообщения по команде /start используя фильтр CommandStart() с меткой <b>{command_args}</b>',
+            reply_markup=main_kb(message.from_user.id))
+
+    else:
+        await message.answer(
+            f'Запуск сообщения по команде /start используя фильтр CommandStart() без метки',
+            reply_markup=main_kb(message.from_user.id))
+
+
+@start_router.message(Command('start_2'))
+async def cmd_start(message: Message):
+    await message.answer('Запуск сообщения по команде /start_2 используя фильтр Command()',
+                         reply_markup=create_spec_kb())
+
+@start_router.message(F.text == '/start_3')
+async def cmd_start_3(message: Message):
+    await message.answer('Запуск сообщения по команде /start_3 используя магический фильтр F.text!')
